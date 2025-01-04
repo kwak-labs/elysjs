39130f6529f94a45c24774b6eb2b33823ef31573
ducksquaddd <crimson.c.moore@gmail.com>
2025-01-04 14:04:52 -0800

    remove exclude

--- packages/elysjs/scripts/codegen.ts
+++ packages/elysjs/scripts/codegen.ts
@@ -22,26 +22,6 @@ export const options: TelescopeInput = {
       excluded: {
         packages: [
           'ibc.applications.fee.v1', // issue with parsing protos (LCD routes with nested objects in params)
-          'cosmos.app.v1alpha1',
-          'cosmos.app.v1beta1',
-          'cosmos.base.kv.v1beta1',
-          'cosmos.base.reflection.v1beta1',
-          'cosmos.base.snapshots.v1beta1',
-          'cosmos.base.store.v1beta1',
-          'cosmos.base.tendermint.v1beta1',
-          'cosmos.crisis.v1beta1',
-          'cosmos.evidence.v1beta1',
-          'cosmos.genutil.v1beta1',
-          'cosmos.autocli.v1',
-          'cosmos.msg.v1',
-          'cosmos.nft.v1beta1',
-          'cosmos.capability.v1beta1',
-          'cosmos.orm.v1alpha1',
-          'cosmos.orm.v1',
-          'cosmos.slashing.v1beta1',
-          'google.api',
-          'ibc.core.port.v1',
-          'ibc.core.types.v1'
         ]
       },
     },
